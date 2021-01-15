3110497dbb3d7df75b791faa1512fb53aa51bbdb
Jingi Kim <jk.kim@fastcampus.co.kr>
2021-01-15 11:00:56 +0900

    Add Movie component & apply CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -15,12 +16,27 @@ function App() {
   }, []);
   
   return (
-    <div className="App">
-      {isLoading ? "Loading..." : movies.map(movie => {
-        console.log(movie);
-        return <Movie key={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
-      })}
-    </div>
+    <section className="container">
+      {isLoading ? (
+        <div class="loader">
+          <span class="loader__text">"Loading..."</span>
+        </div>
+       ) : (
+        <div class="movies">
+          { movies.map(movie => (
+            <Movie 
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </section>
   );
 }
 
